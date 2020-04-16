80f750748dd13f437f041ec0d8879a76de461811
ubuntulim <limys609@naver.com>
2020-04-16 19:00:51 +0900

    hw2-2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include "sorting.h"
+#include <stdio.h>
+#include <iostream>
+
 
 int sorted[10];
 
 int main(){
+
+    int chap[10];
+
+    std::cout << "10개의 정수 입력" <<std::endl;
+
+    for(int j=0;  j<10;j++)
+    std::cin >> chap[j];
+
+    //for(int k=0; k<10; k++)
+    //printf("%d ",sorted[k]);
+    
+    merge_sort(chap,0,9);
+
+
+   for(int k=0; k<10; k++)
+   sorted[k]=chap[k];
+
+
+    for(int i=0; i<10; i++)
+    printf("%d ",sorted[i]);
 
     return 0;
 }
